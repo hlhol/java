5a5112abd33f671873fe817aab04c4278c38f606
7ussain Mohamed <hlhols313@gmail.com>
2023-04-15 14:31:23 +0300

    create a new program to draw shapes with star *

--- src/drawStar.java
+++ src/drawStar.java
@@ -1,11 +1,9 @@
 public class drawstars {
     //intilize a verible call r as row
     //intilize a verible call c as column
-    int c = 6;
 
     for(int r = 0, r<10, r++){
         for(int c = 0, c<=r, c++){
             System.out.print("* ");
-    }
-        System.out.println();
-}
+        }
+    };
