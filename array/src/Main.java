80251802778c0abb142d9975899effc9c3f0361b
7ussain Mohamed <hlhols313@gmail.com>
2023-04-28 10:21:50 +0300

    create java filee for learn array

---
+++ array/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    //thisis how to create a array in java:
+        //declare value of array
+        int [] marks = {15, 1, 5, 7, 9};
+
+        
+
+
+    }
+}
