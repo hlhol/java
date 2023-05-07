59f6c984dabe4b4318ad9ef6d35dc56bf438d398
7ussain Mohamed <hlhols313@gmail.com>
2023-05-07 22:42:17 +0300

    add a file for array

--- array/src/Main.java
+++ array/src/Main.java
@@ -3,8 +3,13 @@ public class Main {
     //thisis how to create a array in java:
         //declare value of array
         int [] marks = {15, 1, 5, 7, 9};
+        int i = 0;
+        boolean flag = false;
 
-        
+        while(i< marks.length && flag ==false){
+            System.out.println(marks[i]);
+            i++;
+        }
 
 
     }
