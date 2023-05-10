b0145cd25a0619c9373b3a6b17e54da000c85f36
7ussain Mohamed <hlhols313@gmail.com>
2023-05-10 19:48:44 +0300

    Finish all quastion in lab6B and create new project to answer lab6C

--- arr-lab6B/src/Main.java
+++ arr-lab6B/src/Main.java
@@ -1,10 +1,62 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        String[] students = {"John","Maryam","Fatema","Paul","Zainab", "Mary", "Ahmed","Peter", "Hussain","Sayed Hussain"};
-        double[] grades = {52.4,67.9,87.3,45.2,77.8,93.6,83.9,62.7,68.9,78.2};
+    Scanner get = new Scanner(System.in);
+
+    //create an array
+        int[] num = new int[4];
+        for(int i = 0; i< num.length; i++){
+            System.out.println("Enter number");
+            num[i] = get.nextInt();
+
+        }
+
+
+        System.out.println("Array content:");
+         for(int j=0; j< num.length; j++){
+
+             System.out.println(num[j]);
+         }
+         double avg = 0;
+
+         for (int k = 0; k< num.length; k++){
+             avg += num[k];
+         }
+
+        System.out.println("Enter Array average: " + avg/ num.length);
+
+
+
+
+
+
+
+
+
+
+
+        //String[] students = {"John", "Maryam", "Fatema", "Paul", "Zainab", "Mary", "Ahmed", "Peter", "Hussain", "Sayed Hussain"};
+        //double[] grades = {52.4, 67.9, 87.3, 45.2, 77.8, 93.6, 83.9, 62.7, 68.9, 78.2};
     }
 
-    public static void searchHighest(String[] students, double[] grades){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*public static void searchHighest(String[] students, double[] grades){
         int i;
         double mark = grades[0];
         String hiname = students[0];
@@ -18,14 +70,14 @@ public class Main {
         }
 
 
-        /*while(i< grades.length){
+        while(i< grades.length){
             if (grades[i]> mark){
                 mark = grades[i];
                 hiname = students[i];
             }
             i++;
         }
-*/
+
         System.out.println( hiname + " got the highest grades of the class: "+ mark);
 
 
@@ -48,6 +100,5 @@ public class Main {
             j++;
         }
 */
-        return total/grades.length;
-    }s
+
 }
