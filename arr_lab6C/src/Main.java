6a8e08b03aaf70240ea53faf9e106362e4625fcd
7ussain Mohamed <hlhols313@gmail.com>
2023-05-11 16:22:57 +0300

    solve q1 in lab6C that one was so hard

--- arr_lab6C/src/Main.java
+++ arr_lab6C/src/Main.java
@@ -9,20 +9,45 @@ public class Main {
         names[4]="Saeed";
         names[5]="Zahra";
         names[6]="Latifa";
+        addName(names);
     }
 
-    public static String[] addName(String[] names){
-
+    public static String[] addName(String[] names) {
         Scanner get = new Scanner(System.in);
+        int i = 0;
+        String resp;
+        while(i<names.length && names[i] != null){
+            i++;
+        }
 
-        for(int i = 0; i< names.length; i++){
-            if(names[i] == null){
+        if (i< names.length){
+            do {
                 System.out.println("Add a student name");
+                names[i] = get.nextLine();
+                i++;
+                System.out.println("Name added successfully. Add another? Enter y or n");
+                resp = get.nextLine();
+            }while(i< names.length && resp.equalsIgnoreCase("y"));
+
+            if(i == names.length){
+                System.out.println("Array full");
+            }
+            System.out.println("Updated Array");
+            for (int j =0; i<j; j++){
+                System.out.println(names[j]);
+            }
+
+        }else{
+            System.out.println("Array full");
+        }
+
+        for(int r = 0; r<names.length; r++){
+            if(names[r] != null){
+                System.out.println(names[r]);
             }
         }
 
-
-
         return names;
     }
+
 }
