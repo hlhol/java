c3588dc67700ca491519c27a846a33fc38d7af87
7ussain Mohamed <hlhols313@gmail.com>
2023-05-12 14:58:20 +0300

    start lab6D but the q1 is answer  and now start q2

---
+++ arr-lab6D/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static int findFirst(int[] vector, int value){
+
+
+
+
+        
+
+
+
+
+
+        return value;
+    }
+}
