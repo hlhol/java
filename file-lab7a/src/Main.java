470f429bd715234def89d60d65247678a7839112
7ussain Mohamed <hlhols313@gmail.com>
2023-05-14 20:54:37 +0300

    complete q2 in lab7A

--- file-lab7a/src/Main.java
+++ file-lab7a/src/Main.java
@@ -21,4 +21,23 @@ public class Main {
                 System.out.println("AbsolutePath : " + inFile.getAbsolutePath());
 
             }
+    public static void listProperties(String name){
+
+
+        File inFile = new File(name);
+
+
+        System.out.println("Printing properties of file "+ name +"\n" +
+                "--------------------------");
+
+        System.out.println("Does it exist : " + inFile.exists());
+        System.out.println("Size in byte : " + inFile.length());
+        System.out.println("Writable : " + inFile.canWrite());
+        System.out.println("Readable : " + inFile.canRead());
+        System.out.println("Is File : " + inFile.isFile());
+        System.out.println("Is Directory : " + inFile.isDirectory());
+        System.out.println("Hidden : " + inFile.isHidden());
+        System.out.println("AbsolutePath : " + inFile.getAbsolutePath());
+    }
+
     }
