a85663af45c4385ec9331c9d9e8067aa827e777a
7ussain Mohamed <hlhols313@gmail.com>
2023-05-14 20:53:38 +0300

    complete 1 in lab7A

---
+++ file-lab7a/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner get = new Scanner(System.in);
+        listProperties("C:\\Users\\202201389\\IdeaProjects\\filename\\src\\file.txt");
+
+        System.out.println("enter a filename");
+        String name = get.nextLine();
+        File inFile = new File(name);
+
+        System.out.println("Printing properties of file\n" +
+                        "--------------------------");
+                System.out.println("Does it exist : " + inFile.exists());
+                System.out.println("Size in byte : " + inFile.length());
+                System.out.println("Writable : " + inFile.canWrite());
+                System.out.println("Readable : " + inFile.canRead());
+                System.out.println("Is File : " + inFile.isFile());
+                System.out.println("Is Directory : " + inFile.isDirectory());
+                System.out.println("Hidden : " + inFile.isHidden());
+                System.out.println("AbsolutePath : " + inFile.getAbsolutePath());
+
+            }
+    }
