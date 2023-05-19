4b17f6b0a747a120038efd4b26ae57d215314c4b
7ussain Mohamed <hlhols313@gmail.com>
2023-05-19 15:20:43 +0300

    complete q3 in lab7a

--- file-lab7a/src/test.java
+++ file-lab7a/src/test.java
@@ -10,46 +10,40 @@ public class test {
         File folder = new File(path);
 
         if(!folder.exists()){
-            System.out.println("Folder does not exist, creating it" );
+            System.out.println("Folder does not exist, creating it");
             folder.mkdir();
         }
 
         System.out.println("");
-        System.out.println("Content of folder before creating new file" );
+        System.out.println("Content of folder before creating new file");
         File[] files = folder.listFiles();
         if (files == null || files.length == 0) {
-            System.out.println("Folder is empty"+ "\n");
-            File newFile = new File(folder, "newfile.txt");
-            newFile.createNewFile();
+            System.out.println("Folder is empty");
             System.out.println("\nCreating new file\n");
-
-            System.out.println("Content of folder after creating new file" );
+            File newFile = new File(folder.getCanonicalPath() + File.separator + "newfile.txt");
+            newFile.createNewFile();
+            System.out.println("Content of folder after creating new file");
             for (String fileName : folder.list()){
                 System.out.println(fileName);
             }
-
-
-
         } else {
-
-
-
             for (String fileName : folder.list()){
                 System.out.println(fileName);
             }
 
-            File newFile = new File(folder, "newfile.txt");
-            newFile.createNewFile();
-            System.out.println("\nCreating new file\n");
+            File newFile = new File(folder.getCanonicalPath() + File.separator + "newfile.txt");
+            if(!newFile.exists()){
+                newFile.createNewFile();
+                System.out.println("\nCreating new file\n");
+            }
+            else{
+                System.out.println("File Exists Already");
+            }
 
-            System.out.println("Content of folder after creating new file" );
+            System.out.println("Content of folder after creating new file");
             for (String fileName : folder.list()){
                 System.out.println(fileName);
             }
         }
-
-        }
-
-
-
     }
+}
