aa8562406a0552400fe70e414f3c35cf285ca6b0
7ussain Mohamed <hlhols313@gmail.com>
2023-06-11 16:14:31 +0300

    add lab8a

---
+++ lab8a/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static int Add(int num1, int num2){
+
+
+        return num1 + num2;
+    }
+
+    public static int subtract(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public static int multiply(int num1, int num2){
+
+        return num1 * num2;
+    }
+
+
+    public static int divide(int num1, int num2){
+
+
+        return num1 / num2;
+    }
+
+}
